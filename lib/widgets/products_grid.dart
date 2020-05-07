60fd65b78e5c4ff140d60f553bbdc5ccf04197a7
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-05-07 12:38:29 +0300

    better use .value constructor of ChangeNotifierProvider

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -11,8 +11,8 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (context, index) => ChangeNotifierProvider(
-        builder:(ctx)=> products[index],
+      itemBuilder: (context, index) => ChangeNotifierProvider.value(
+        value: products[index],
         child: ProductItem(),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
