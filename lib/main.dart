1ea685936c6c751740aff3ff6f4d09fb57599a23
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-05-07 18:45:56 +0300

    setup multiple providers in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import './providers/cart.dart';
 import './providers/products_provider.dart';
 import './pages/product_detail_page.dart';
 import './pages/products_overview_page.dart';
 import 'package:provider/provider.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -12,17 +12,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-     value: Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: Products(),
+        ),
+        ChangeNotifierProvider.value(value: Cart()),
+      ],
       child: MaterialApp(
         title: 'PetShop',
         theme: ThemeData(
             primarySwatch: Colors.orange,
-        fontFamily: 'Lato',
-        accentColor: Colors.deepOrange),
+            fontFamily: 'Lato',
+            accentColor: Colors.deepOrange),
         home: ProductsOverviewPage(),
         routes: {
-          ProductDetailPage.routeName: (ctx)=>ProductDetailPage(),
+          ProductDetailPage.routeName: (ctx) => ProductDetailPage(),
         },
       ),
     );
