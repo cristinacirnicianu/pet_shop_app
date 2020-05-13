f9ace6d69adee1a05ef82e94c0c4535b9c260b1f
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-05-13 11:05:21 +0300

    use snack bar to give undo action after adding item to cart

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/foundation.dart';
 
 class CartItem {
-final String id;
-final String title;
-final int quantity;
-final double price;
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
 
-CartItem({@required this.id,@required  this.title,@required  this.quantity,@required  this.price});
+  CartItem(
+      {@required this.id,
+      @required this.title,
+      @required this.quantity,
+      @required this.price});
 }
 
 class Cart with ChangeNotifier {
@@ -16,34 +20,35 @@ class Cart with ChangeNotifier {
     return {..._items};
   }
 
-  double get totalAmount{
-    var total =0.0;
+  double get totalAmount {
+    var total = 0.0;
     _items.forEach((key, cartItem) {
       total += cartItem.price * cartItem.quantity;
     });
     return total;
   }
 
-  int get itemCount{
+  int get itemCount {
     return _items.length;
   }
 
-  void addItem (String productId, double price, String title){
-    if(_items.containsKey(productId)){
-      _items.update(productId,
-              (existingCartItem) => CartItem(
-                  id: existingCartItem.id,
-                  title: existingCartItem.title,
-                  quantity: existingCartItem.quantity +1,
-                  price: existingCartItem.price));
-
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+              id: existingCartItem.id,
+              title: existingCartItem.title,
+              quantity: existingCartItem.quantity + 1,
+              price: existingCartItem.price));
     } else {
-      _items.putIfAbsent(productId,
-      ()=> CartItem(
-        id: DateTime.now().toString(),
-      title: title,
-      price: price,
-      quantity: 1));
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              title: title,
+              price: price,
+              quantity: 1));
     }
     notifyListeners();
   }
@@ -52,8 +57,27 @@ class Cart with ChangeNotifier {
     _items.remove(productId);
     notifyListeners();
   }
-  void clear () {
+
+  void clear() {
     _items = {};
+    notifyListeners();
+  }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId].quantity > 1) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+              id: existingCartItem.id,
+              title: existingCartItem.title,
+              price: existingCartItem.price,
+              quantity: existingCartItem.quantity - 1));
+    } else {
+      _items.remove(productId);
+    }
     notifyListeners();
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -5,34 +5,59 @@ import 'package:provider/provider.dart';
 import '../pages/product_detail_page.dart';
 
 class ProductItem extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
-    final cart = Provider.of<Cart>(context, listen:  false);
+    final cart = Provider.of<Cart>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
-          onTap: (){
-            Navigator.of(context).pushNamed(ProductDetailPage.routeName, arguments: product.id);
-          },
-            child: Image.network(product.imageUrl, fit: BoxFit.cover,)),
-        footer: GridTileBar(title: Text(product.title, textAlign: TextAlign.center,),
-          leading: Consumer<Product>(
-            builder: (ctx, product, _) => IconButton(icon: Icon( product.isFavorite ? Icons.favorite : Icons.favorite_border), onPressed: (){
-              product.toggleFavorite();
-            }, color: Theme.of(context).accentColor,),
+            onTap: () {
+              Navigator.of(context).pushNamed(ProductDetailPage.routeName,
+                  arguments: product.id);
+            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            )),
+        footer: GridTileBar(
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
           ),
-          trailing: IconButton(icon: Icon(Icons.shopping_cart), 
-            onPressed: (){
-            cart.addItem(product.id, product.price, product.title);
-            Scaffold.of(context).showSnackBar(
-                SnackBar(
-                  content: Text('Added item to cart!', textAlign: TextAlign.center,),
-                duration: Duration(seconds: 2),));
-          }, color: Theme.of(context).accentColor,),
-          backgroundColor: Colors.black87,),
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              onPressed: () {
+                product.toggleFavorite();
+              },
+              color: Theme.of(context).accentColor,
+            ),
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text(
+                  'Added item to cart!',
+                ),
+                duration: Duration(seconds: 2),
+                action: SnackBarAction(
+                  label: 'UNDO',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+              ));
+            },
+            color: Theme.of(context).accentColor,
+          ),
+          backgroundColor: Colors.black87,
+        ),
       ),
     );
   }
