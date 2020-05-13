3db02f52fd0944735778bf24c6cc5acdd373432d
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-05-13 12:01:30 +0300

    add user product item to user products page

--- lib/pages/user_products_page.dart
+++ lib/pages/user_products_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:petshopapp/providers/products_provider.dart';
+import 'package:petshopapp/widgets/user_product_item.dart';
 import 'package:provider/provider.dart';
 
 class UserProductsPage extends StatelessWidget {
@@ -10,13 +11,19 @@ class UserProductsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Your products'),
         actions: [
-          IconButton(icon: const Icon(Icons.add), onPressed: () {},),
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
         ],
       ),
       body: Padding(
         padding: EdgeInsets.all(8),
-        child: ListView.builder(itemCount: productData.items.length ,
-            itemBuilder: (_, index) =>),
+        child: ListView.builder(
+            itemCount: productData.items.length,
+            itemBuilder: (_, index) => UserProductItem(
+                productData.items[index].title,
+                productData.items[index].imageUrl)),
       ),
     );
   }
