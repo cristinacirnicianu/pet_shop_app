bcb5bee06d33ef097888ccb97daa9915a15297ac
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-05-13 16:22:52 +0300

    user dispose method in order to avoid memory leak

--- lib/pages/edit_product_page.dart
+++ lib/pages/edit_product_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class EditProductPage extends StatefulWidget {
   static const routeName = '/edit-product';
+
   @override
   _EditProductPageState createState() => _EditProductPageState();
 }
@@ -9,40 +10,51 @@ class EditProductPage extends StatefulWidget {
 class _EditProductPageState extends State<EditProductPage> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Edit product'),),
+      appBar: AppBar(
+        title: Text('Edit product'),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Form(child: ListView(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Title'),
-              textInputAction: TextInputAction.next,
-              onFieldSubmitted: (_) {
-                FocusScope.of(context).requestFocus(_priceFocusNode);
-              },
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Price'),
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.number,
-              focusNode: _priceFocusNode,
-              onFieldSubmitted: (_) {
-                FocusScope.of(context).requestFocus(_descriptionFocusNode);
-              },
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Description'),
-              maxLines: 3,
-              focusNode: _descriptionFocusNode,
-              keyboardType: TextInputType.multiline,
-            ),
-          ],
-        ),),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                focusNode: _descriptionFocusNode,
+                keyboardType: TextInputType.multiline,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-
