140155b5bd185dc971f02b871aa1de358b28adca
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-05-19 17:32:28 +0300

    update favorites status, optimistically

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Product extends ChangeNotifier{
+class Product extends ChangeNotifier {
   final String id;
   final String title;
   final String description;
@@ -8,17 +10,34 @@ class Product extends ChangeNotifier{
   final String imageUrl;
   bool isFavorite;
 
-  Product({
-   @required this.id,
-    @required this.title,
-    @required this.description,
-    @required this.price,
-    @required this.imageUrl,
-    this.isFavorite = false});
+  Product(
+      {@required this.id,
+      @required this.title,
+      @required this.description,
+      @required this.price,
+      @required this.imageUrl,
+      this.isFavorite = false});
 
-  void toggleFavorite(){
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavorite() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
-
+    final url = 'https://flutter-demo-fire.firebaseio.com/products/$id.json';
+    try {
+      final response = await http.patch(url,
+          body: json.encode({
+            'isFavorite': isFavorite,
+          }));
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
