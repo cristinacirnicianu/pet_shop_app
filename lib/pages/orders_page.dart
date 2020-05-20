ac7340be5a391238357ff1293df192d4c99d8b4f
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-05-20 09:53:05 +0300

    improve the code by using FutureBuilder

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -4,43 +4,31 @@ import '../widgets/app_drawer.dart';
 import '../widgets/order_item.dart';
 import 'package:provider/provider.dart';
 
-class OrdersPage extends StatefulWidget {
+class OrdersPage extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  _OrdersPageState createState() => _OrdersPageState();
-}
-
-class _OrdersPageState extends State<OrdersPage> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    _isLoading = true;
-
-    Provider.of<Orders>(context, listen: false)
-        .fetchAndSetOrders()
-        .then((_) => {
-              setState(() {
-                _isLoading = false;
-              })
-            });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final ordersData = Provider.of<Orders>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Your orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: ordersData.orders.length,
-              itemBuilder: (ctx, index) => OrderItem(ordersData.orders[index])),
+      body:FutureBuilder(future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(), builder: (ctx, dataProvider) {
+        if(dataProvider.connectionState ==ConnectionState.waiting){
+          return Center(child: CircularProgressIndicator());
+        } else {
+          if(dataProvider.error != null){
+            //do error code
+            return Center(child: Text('An error ocurred'),);
+          } else {
+            return Consumer<Orders>(builder: (ctx, ordersData, child) => ListView.builder(
+                itemCount: ordersData.orders.length,
+                itemBuilder: (ctx, index) => OrderItem(ordersData.orders[index])),);
+          }
+        }
+      },),
     );
   }
 }
