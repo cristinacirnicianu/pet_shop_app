62cc8d215f5e52dffea7c7c99db80d189eb48214
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-05-20 09:36:09 +0300

    alternative to fetch data

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -8,37 +8,39 @@ class OrdersPage extends StatefulWidget {
   static const routeName = '/orders';
 
   @override
-  _OrdersPageState createState()=>_OrdersPageState();
-
+  _OrdersPageState createState() => _OrdersPageState();
 }
 
 class _OrdersPageState extends State<OrdersPage> {
   var _isLoading = false;
+
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-     await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
+    _isLoading = true;
+
+    Provider.of<Orders>(context, listen: false)
+        .fetchAndSetOrders()
+        .then((_) => {
+              setState(() {
+                _isLoading = false;
+              })
+            });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-
     final ordersData = Provider.of<Orders>(context);
     return Scaffold(
-      appBar: AppBar(title: Text('Your orders'),),
+      appBar: AppBar(
+        title: Text('Your orders'),
+      ),
       drawer: AppDrawer(),
-      body:_isLoading ?Center(child: CircularProgressIndicator()) : ListView.builder(itemCount: ordersData.orders.length,
-          itemBuilder: (ctx, index)=>OrderItem(ordersData.orders[index])),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: ordersData.orders.length,
+              itemBuilder: (ctx, index) => OrderItem(ordersData.orders[index])),
     );
   }
-
-
 }
