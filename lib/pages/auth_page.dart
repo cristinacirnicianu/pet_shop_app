0f5bb6bf17abddd24d91d3b2b100aff31f0bba69
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-05-25 17:28:40 +0300

    more animation and transitions widget

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -12,7 +12,9 @@ class AuthPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final deviceSize = MediaQuery.of(context).size;
+    final deviceSize = MediaQuery
+        .of(context)
+        .size;
     // final transformConfig = Matrix4.rotationZ(-8 * pi / 180);
     // transformConfig.translate(-10.0);
     return Scaffold(
@@ -44,7 +46,7 @@ class AuthPage extends StatelessWidget {
                     child: Container(
                       margin: EdgeInsets.only(bottom: 20.0),
                       padding:
-                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 94.0),
+                      EdgeInsets.symmetric(vertical: 8.0, horizontal: 94.0),
                       transform: Matrix4.rotationZ(-8 * pi / 180)
                         ..translate(-10.0),
                       // ..translate(-10.0),
@@ -62,7 +64,11 @@ class AuthPage extends StatelessWidget {
                       child: Text(
                         'PetShop',
                         style: TextStyle(
-                          color: Theme.of(context).accentTextTheme.title.color,
+                          color: Theme
+                              .of(context)
+                              .accentTextTheme
+                              .title
+                              .color,
                           fontSize: 50,
                           fontFamily: 'Anton',
                           fontWeight: FontWeight.normal,
@@ -105,7 +111,8 @@ class _AuthCardState extends State<AuthCard>
   var _isLoading = false;
   final _passwordController = TextEditingController();
   AnimationController _controller;
-  Animation<Size> _heightAnimation;
+  Animation<Offset> _slideAnimation;
+  Animation<double> _animationOpacity;
 
   @override
   void initState() {
@@ -115,10 +122,13 @@ class _AuthCardState extends State<AuthCard>
         duration: Duration(
           milliseconds: 300,
         ));
-    _heightAnimation = Tween<Size>(
-            begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
-        .animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
-    _heightAnimation.addListener(() => setState(() {}));
+    _slideAnimation = Tween<Offset>(
+        begin: Offset(0, -1.5), end: Offset(0, 0))
+        .animate(
+        CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn));
+    //  _heightAnimation.addListener(() => setState(() {}));
+    _animationOpacity = Tween(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(parent: _controller, curve: Curves.easeIn));
   }
 
   @override
@@ -130,7 +140,8 @@ class _AuthCardState extends State<AuthCard>
   void _showErrorDialog(String message) {
     showDialog(
         context: context,
-        builder: (ctx) => AlertDialog(
+        builder: (ctx) =>
+            AlertDialog(
               title: Text('An error occurred!'),
               content: Text(message),
               actions: [
@@ -202,7 +213,9 @@ class _AuthCardState extends State<AuthCard>
 
   @override
   Widget build(BuildContext context) {
-    final deviceSize = MediaQuery.of(context).size;
+    final deviceSize = MediaQuery
+        .of(context)
+        .size;
     return Card(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
@@ -210,13 +223,13 @@ class _AuthCardState extends State<AuthCard>
       elevation: 8.0,
       child: AnimatedContainer(
         duration: Duration(milliseconds: 300),
-            curve: Curves.easeIn,
-            height: _authMode == AuthMode.Signup ? 320 : 260,
-          //  height: _heightAnimation.value.height,
-            constraints:
-                BoxConstraints(minHeight: _authMode==AuthMode.Signup ? 320 : 260),
-            width: deviceSize.width * 0.75,
-            padding: EdgeInsets.all(16.0),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        //  height: _heightAnimation.value.height,
+        constraints:
+        BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -225,7 +238,7 @@ class _AuthCardState extends State<AuthCard>
                 TextFormField(
                   decoration: InputDecoration(labelText: 'E-Mail'),
                   keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
+                  validator:  (value) {
                     if (value.isEmpty || !value.contains('@')) {
                       return 'Invalid email!';
                     }
@@ -247,19 +260,32 @@ class _AuthCardState extends State<AuthCard>
                     _authData['password'] = value;
                   },
                 ),
-                if (_authMode == AuthMode.Signup)
-                  TextFormField(
-                    enabled: _authMode == AuthMode.Signup,
-                    decoration: InputDecoration(labelText: 'Confirm Password'),
-                    obscureText: true,
-                    validator: _authMode == AuthMode.Signup
-                        ? (value) {
-                            if (value != _passwordController.text) {
-                              return 'Passwords do not match!';
-                            }
+                //   if (_authMode == AuthMode.Signup)
+                AnimatedContainer(
+                  constraints: BoxConstraints(
+                      minHeight: _authMode == AuthMode.Signup ? 60 :0,
+                      maxHeight: _authMode==AuthMode.Signup ? 120 : 0),
+                  duration: Duration(milliseconds: 300),
+                  curve: Curves.easeIn,
+                  child: FadeTransition(
+                    opacity: _animationOpacity,
+                    child: SlideTransition(
+                      position: _slideAnimation,
+                      child: TextFormField(
+                        enabled: _authMode == AuthMode.Signup,
+                        decoration: InputDecoration(labelText: 'Confirm Password'),
+                        obscureText: true,
+                        validator: _authMode == AuthMode.Signup
+                            ? (value) {
+                          if (value != _passwordController.text) {
+                            return 'Passwords do not match!';
                           }
-                        : null,
+                        }
+                            : null,
+                      ),
+                    ),
                   ),
+                ),
                 SizedBox(
                   height: 20,
                 ),
@@ -268,15 +294,21 @@ class _AuthCardState extends State<AuthCard>
                 else
                   RaisedButton(
                     child:
-                        Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
+                    Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
                     onPressed: _submit,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(30),
                     ),
                     padding:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
-                    color: Theme.of(context).primaryColor,
-                    textColor: Theme.of(context).primaryTextTheme.button.color,
+                    EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+                    color: Theme
+                        .of(context)
+                        .primaryColor,
+                    textColor: Theme
+                        .of(context)
+                        .primaryTextTheme
+                        .button
+                        .color,
                   ),
                 FlatButton(
                   child: Text(
@@ -284,7 +316,9 @@ class _AuthCardState extends State<AuthCard>
                   onPressed: _switchAuthMode,
                   padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
                   materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  textColor: Theme.of(context).primaryColor,
+                  textColor: Theme
+                      .of(context)
+                      .primaryColor,
                 ),
               ],
             ),
